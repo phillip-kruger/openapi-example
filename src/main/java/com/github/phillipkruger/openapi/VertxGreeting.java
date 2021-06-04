4574da6c6b1cce6c175412d97dd9e8ef307246c6
Phillip Kruger <phillip.kruger@gmail.com>
2021-06-04 14:06:48 +0200

    Update to CR3 Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- src/main/java/com/github/phillipkruger/openapi/VertxGreeting.java
+++ src/main/java/com/github/phillipkruger/openapi/VertxGreeting.java
@@ -6,7 +6,7 @@ import io.quarkus.vertx.web.Param;
 import javax.enterprise.context.ApplicationScoped;
 import io.quarkus.vertx.web.Route;
 import io.quarkus.vertx.web.RouteBase;
-import io.vertx.core.http.HttpMethod;
+import static io.quarkus.vertx.web.Route.HttpMethod.*;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @ApplicationScoped
@@ -14,17 +14,17 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 @Tag(name = "Vert.x Resource", description = "Basic Hello World using Vert.x")
 public class VertxGreeting {
 
-    @Route(path = "/hello", methods = HttpMethod.GET)
+    @Route(path = "/hello", methods = GET)
     public Greeting helloVertX() { 
         return new Greeting("Hello", "Vert.x");
     }
     
-    @Route(path = "/hello", methods = HttpMethod.POST)
+    @Route(path = "/hello", methods = POST)
     public Greeting newHelloVertX(@Body Greeting greeting) {
         return greeting;
     }
     
-    @Route(path = "/hello/:message", methods = HttpMethod.DELETE)
+    @Route(path = "/hello/:message", methods = DELETE)
     public boolean deleteHelloVertX(@Param("message") String message) {
         return true;
     }
